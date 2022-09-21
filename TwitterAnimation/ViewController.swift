d83dfd7cf4600f2fd52151add217eb5ce6fcb31e
Dmitrii Kim <mitya_kim@icloud.com>
2022-09-21 12:58:47 -0700

    Initial Commit

---
+++ TwitterAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterAnimation
+//
+//  Created by Mitya Kim on 9/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
