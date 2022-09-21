977248909cd89be040f9bf58d8fdcc7be4308269
Dmitrii Kim <mitya_kim@icloud.com>
2022-09-21 13:29:29 -0700

    Twitter animation

--- TwitterAnimation/ViewController.swift
+++ TwitterAnimation/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let image: UIImageView = {
+        let image = UIImageView()
+        image.isUserInteractionEnabled = true
+        image.image = UIImage(named: "tile00")
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(image)
+        
+        image.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(animate)))
+        
+        image.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        image.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        image.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        image.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        for i in 0..<29 {
+            spriteImages.append(UIImage(named: "tile0\(i)")!)
+        }
     }
-
-
+    
+    var spriteImages = [UIImage]()
+    
+    @objc func animate() {
+        image.animationImages = spriteImages
+        image.animationDuration = 0.6
+        image.animationRepeatCount = 1
+        image.startAnimating()
+    }
 }
 
