fb7f8f55c081149fce7bd83ec7d092003173d76b
Dmitrii Kim <mitya_kim@icloud.com>
2022-09-26 16:01:32 -0700

    Initial Commit

---
+++ NotificationAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotificationAnimation
+//
+//  Created by Mitya Kim on 9/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
