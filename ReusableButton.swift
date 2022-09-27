8ad8fd407077431e58f507a2648e9d2d4c072eed
Dmitrii Kim <mitya_kim@icloud.com>
2022-09-26 17:24:10 -0700

    Initial Commit

---
+++ ReusableButton.swift
@@ -0,0 +1,82 @@
+//
+//  ReusableButton.swift
+//  NotificationAnimation
+//
+//  Created by Mitya Kim on 9/26/22.
+//
+
+import Foundation
+
+import UIKit
+
+class ReusableButton: UIButton {
+    
+    fileprivate var wAnchor: NSLayoutConstraint?
+    fileprivate var hAnchor: NSLayoutConstraint?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        phaseTwo(title: "Button")
+    }
+
+    init(title: String) {
+        super.init(frame: .zero)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        wAnchor = widthAnchor.constraint(equalToConstant: 150)
+        wAnchor?.isActive = true
+        
+        hAnchor = heightAnchor.constraint(equalToConstant: 54)
+        guard let hAnchor = hAnchor else { return }
+        hAnchor.isActive = true
+        phaseTwo(title: title)
+    }
+    
+    fileprivate func phaseTwo(title: String) {
+        
+        layer.cornerRadius = 12
+        backgroundColor = .darkGray
+        setTitle(title, for: .normal)
+        titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+//        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(animateButton)))
+        addTarget(self, action: #selector(down), for: .touchDown)
+        addTarget(self, action: #selector(up), for: .touchUpInside)
+    }
+    
+    @objc fileprivate func down() {
+  
+        wAnchor?.isActive = false
+        hAnchor?.isActive = false
+        wAnchor?.constant = 150
+        hAnchor?.constant = 40
+        wAnchor?.isActive = true
+        hAnchor?.isActive = true
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            self.superview?.layoutIfNeeded()
+//            self.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        })
+    }
+    
+    
+    @objc fileprivate func up() {
+        
+        wAnchor?.isActive = false
+        hAnchor?.isActive = false
+        wAnchor?.constant = 150
+        hAnchor?.constant = 54
+        wAnchor?.isActive = true
+        hAnchor?.isActive = true
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            self.superview?.layoutIfNeeded()
+//            self.transform = .identity
+        })
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
